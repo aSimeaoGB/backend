8ca52e900a2b6edd400ee9fadcf39a9802b8f6ce
aSimeaoGB <asimeaogb@gmail.com>
2024-10-25 11:43:23 -0300

    Add files via upload

--- CopyFile.java
+++ CopyFile.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import static java.nio.file.StandardCopyOption.*;
-import java.nio.file.*; // nio = imput e output
+import java.nio.file.*;
 
 public class CopyFile {
     public static void copiarArquivo() {
@@ -12,9 +12,34 @@ public class CopyFile {
             int returnVal1 = chooser.showOpenDialog(null);
             String fileFullPath = "";
             String fileName = "";
-            if (returnVal1 == JFileChooser.APPROVE_OPTION) {
+            if(returnVal1 == JFileChooser.APPROVE_OPTION) {
                 fileFullPath = chooser.getSelectedFile().getAbsolutePath();
+                fileName = chooser.getSelectedFile().getName();
+            } else {
+                System.out.println("Que pena!");
             }
+
+            chooser.setDialogTitle("Selecione a pasta de destino.");
+            chooser.setApproveButtonText("Colar aqui");
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int returnVal2 = chooser.showOpenDialog(null);
+            String folderFullPath = "";
+            if(returnVal2 == JFileChooser.APPROVE_OPTION) {
+                folderFullPath = chooser.getSelectedFile().getAbsolutePath();
+            } else {
+                System.out.println("Que pena!");
+            }
+
+            Path pathOrigin = Paths.get(fileFullPath);
+            Path pathDestination = Paths.get(folderFullPath + "\\" + fileName);
+            if (fileFullPath.length() > 0 && folderFullPath.length() > 0) {
+                Files.copy(pathOrigin, pathDestination, REPLACE_EXISTING);
+                System.out.println("Arquivo " + fileName + " copiado/colado com sucesso!");
+            } else {
+                System.out.println("Ops! Não foi possível copiar o arquivo. Por favor, verifique e tente novamente.");
+            }
+        } catch (Exception e) {
+            System.err.println("Não foi possível copiar o arquivo! Tente novamente mais tarde.");
         }
     }
 }
