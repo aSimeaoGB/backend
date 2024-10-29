0b6495b95c84a4583273f6b112e0365a01abbbf2
aSimeaoGB <asimeaogb@gmail.com>
2024-10-29 09:09:30 -0300

    Add files via upload

---
+++ RenameFile.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import static java.nio.file.StandardCopyOption.*;
+import java.nio.file.*;
+
+public class RenameFile {
+    public static void renomearArquivo() {
+        try {
+            JFileChooser chooser = new JFileChooser();
+
+            chooser.setDialogTitle("Selecione o arquivo que deseja renomear");
+            chooser.setApproveButtonText("Selecionar arquivo");
+            int returnVal1 = chooser.showOpenDialog(null);
+            String fileFullPath = "";
+            String folderFullPath = "";
+            String fileName = "";
+
+            String fileAeatoryName = "file-" + String.valueOf(Math.random());
+
+            if(returnVal1 == JFileChooser.APPROVE_OPTION) {
+                fileFullPath = chooser.getSelectedFile().getAbsolutePath();
+                folderFullPath = chooser.getSelectedFile().getParent();
+                fileName = chooser.getSelectedFile().getName();
+            } else {
+                System.out.println("Que pena! Ok, saindo...");
+            }
+
+            String newFileName = JOptionPane.showInputDialog(null, "Digite o novo nome do arquivo", fileName);
+
+            Path pathOrigin = Paths.get(fileFullPath);
+            Path pathDestination = Paths.get(folderFullPath + "\\" + newFileName);
+
+            Path pathTemp = Paths.get(folderFullPath + "\\" + fileAeatoryName);
+
+            if (
+                fileFullPath.length() > 0 && 
+                folderFullPath.length() > 0 &&
+                fileName.length() > 0 &&
+                newFileName.length() > 0
+            ) {
+                Files.copy(pathOrigin, pathDestination, REPLACE_EXISTING);
+                Files.delete(pathOrigin);
+                Files.copy(pathTemp, pathDestination, REPLACE_EXISTING);
+                Files.delete(pathTemp);
+
+                if (fileName.toLowerCase().equals(newFileName.toLowerCase()) == false) {
+                    Files.delete(pathOrigin);
+                }
+                System.out.println("Arquivo " + chooser.getSelectedFile().getName() + " renomeado com sucesso.");
+            } else {
+                System.out.println("Ops! Não foi possível renomear o arquivo. Por favor, verifique e tente novamente mais tarde.");
+            }
+        } catch (Exception e) {
+            System.out.println("Não foi possível renomear o arquivo. Erro: " + e);
+        }
+    }
+}
